71d90de9d51d0857cc587491d78a9868e4a2a868
eshitaarora <78889909+eshitaarora@users.noreply.github.com>
2023-06-25 20:12:24 +0530

    Create app.rb

---
+++ app.rb
@@ -0,0 +1,32 @@
+# app.rb
+
+require 'sinatra'
+require 'securerandom'
+
+# In-memory data store to hold the URL mappings
+$url_mappings = {}
+
+get '/' do
+  erb :index
+end
+
+post '/shorten' do
+  long_url = params[:long_url]
+  short_url = generate_short_url
+
+  $url_mappings[short_url] = long_url
+
+  erb :shortened, locals: { short_url: short_url }
+end
+
+get '/:short_url' do
+  short_url = params[:short_url]
+  long_url = $url_mappings[short_url]
+
+  redirect long_url || '/'
+end
+
+def generate_short_url
+  # Generate a unique short URL using a random alphanumeric string
+  SecureRandom.urlsafe_base64(6)
+end
