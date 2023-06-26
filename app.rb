cdbd07cdb6dd4e4531e91f7a3df40f9c4d8b6138
eshitaarora <78889909+eshitaarora@users.noreply.github.com>
2023-06-26 18:35:28 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,47 +1,38 @@
-#app.rb
+# app.rb
 
 require 'sinatra'
 require 'securerandom'
-# app.rb
 
-# app.rb
+# Data structure to store URL mappings
+URL_MAP = {}
 
-require 'sinatra/base'
-require 'webrick'
-
-# Set the web server handler explicitly to WEBrick
-configure { |c| c.server = 'webrick' }
-
-# Rest of your code...
-
-# Run the application
-Sinatra::Application.run!
-
-
-# In-memory data store to hold the URL mappings
-$url_mappings = {}
-
+# Home page
 get '/' do
   erb :index
 end
 
+# Shorten URL
 post '/shorten' do
   long_url = params[:long_url]
   short_url = generate_short_url
-
-  $url_mappings[short_url] = long_url
-
+  URL_MAP[short_url] = long_url
   erb :shortened, locals: { short_url: short_url }
 end
 
-get '/:short_url' do
-  short_url = params[:short_url]
-  long_url = $url_mappings[short_url]
-
-  redirect long_url || '/'
+# Redirect to the original URL
+get '/:short_url' do |short_url|
+  long_url = URL_MAP[short_url]
+  if long_url
+    redirect long_url
+  else
+    'Short URL not found'
+  end
 end
 
+# Helper method to generate a unique short URL
 def generate_short_url
-  # Generate a unique short URL using a random alphanumeric string
-  SecureRandom.urlsafe_base64(6)
+  loop do
+    short_url = SecureRandom.urlsafe_base64(6)[0...6]
+    return short_url unless URL_MAP.key?(short_url)
+  end
 end
